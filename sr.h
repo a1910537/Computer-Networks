d510c6dbacaa87f30bc81455aa555a6bd7e7e159
1999051 <141125113+1999051@users.noreply.github.com>
2025-05-03 16:29:12 +0930

    Update sr.h

--- sr.h
+++ sr.h
@@ -1,16 +1,17 @@
 #ifndef SR_H
 #define SR_H
 
-extern void A_init(void);
-extern void B_init(void);
-extern void A_output(struct msg);
-extern void B_input(struct pkt);
-extern void A_input(struct pkt);
-extern void A_timerinterrupt(void);
+#include "emulator.h"
 
-/* For bi-directional extension (not used in this assignment) */
-#define BIDIRECTIONAL 0  /* 0 = unidirectional A->B, 1 = bidirectional (not used) */
-extern void B_output(struct msg);
-extern void B_timerinterrupt(void);
+#define WINDOW_SIZE 8
+#define SEQSPACE (2 * WINDOW_SIZE)
+
+void A_init();
+void A_input(struct pkt packet);
+void A_output(struct msg message);
+void A_timerinterrupt();
+
+void B_init();
+void B_input(struct pkt packet);
 
 #endif
