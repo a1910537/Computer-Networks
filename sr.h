5543640f1719ee6880964bb46362584bbcb90f41
1999051 <141125113+1999051@users.noreply.github.com>
2025-05-03 16:34:45 +0930

    Update sr.h

--- sr.h
+++ sr.h
@@ -4,11 +4,11 @@
 #include "emulator.h"
 
 #define WINDOW_SIZE 8
-#define SEQSPACE (2 * WINDOW_SIZE)
+#define TIMEOUT 20.0
 
 void A_init();
-void A_input(struct pkt packet);
 void A_output(struct msg message);
+void A_input(struct pkt packet);
 void A_timerinterrupt();
 
 void B_init();
