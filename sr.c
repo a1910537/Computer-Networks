2f5e580037676354fd846442b58d58d1f44fd9cc
1999051 <141125113+1999051@users.noreply.github.com>
2025-05-03 16:42:27 +0930

    Update sr.c

--- sr.c
+++ sr.c
@@ -1,12 +1,15 @@
+#include <stddef.h>  // 为了使用 NULL
 #include "sr.h"
 
-static struct pkt A_buffer[WINDOW_SIZE];
-static int A_ack[WINDOW_SIZE];
+#define SEQSPACE (2 * WINDOW_SIZE)
+
+static struct pkt A_buffer[SEQSPACE];
+static int A_ack[SEQSPACE];
 static int A_base = 0;
 static int A_nextseq = 0;
 
-static struct pkt B_buffer[WINDOW_SIZE];
-static int B_received[WINDOW_SIZE];
+static struct pkt B_buffer[SEQSPACE];
+static int B_received[SEQSPACE];
 static int B_expected = 0;
 
 int compute_checksum(struct pkt *packet) {
@@ -18,11 +21,11 @@ int compute_checksum(struct pkt *packet) {
     return checksum;
 }
 
-struct pkt make_pkt(int seq, int ack, char *data) {
+struct pkt make_pkt(int seq, int ack, const char *data) {
     struct pkt packet;
+    int i;
     packet.seqnum = seq;
     packet.acknum = ack;
-    int i;
     for (i = 0; i < 20; i++) {
         packet.payload[i] = data ? data[i] : 0;
     }
@@ -33,7 +36,7 @@ struct pkt make_pkt(int seq, int ack, char *data) {
 void send_window() {
     int i;
     for (i = 0; i < WINDOW_SIZE; i++) {
-        int idx = (A_base + i) % WINDOW_SIZE;
+        int idx = (A_base + i) % SEQSPACE;
         if (!A_ack[idx] && A_buffer[idx].seqnum >= 0) {
             tolayer3(0, A_buffer[idx]);
         }
@@ -43,7 +46,7 @@ void send_window() {
 
 void A_init() {
     int i;
-    for (i = 0; i < WINDOW_SIZE; i++) {
+    for (i = 0; i < SEQSPACE; i++) {
         A_buffer[i].seqnum = -1;
         A_ack[i] = 0;
     }
@@ -52,12 +55,12 @@ void A_init() {
 }
 
 void A_output(struct msg message) {
-    if ((A_nextseq + WINDOW_SIZE - A_base) % WINDOW_SIZE >= WINDOW_SIZE) {
+    if ((A_nextseq + SEQSPACE - A_base) % SEQSPACE >= WINDOW_SIZE) {
         return;
     }
 
     struct pkt packet = make_pkt(A_nextseq, -1, message.data);
-    int idx = A_nextseq % WINDOW_SIZE;
+    int idx = A_nextseq % SEQSPACE;
     A_buffer[idx] = packet;
     A_ack[idx] = 0;
 
@@ -66,20 +69,20 @@ void A_output(struct msg message) {
         starttimer(0, TIMEOUT);
     }
 
-    A_nextseq = (A_nextseq + 1) % (WINDOW_SIZE * 2);
+    A_nextseq = (A_nextseq + 1) % SEQSPACE;
 }
 
 void A_input(struct pkt packet) {
     int checksum = compute_checksum(&packet);
     if (checksum != packet.checksum || packet.acknum < 0) return;
 
-    int idx = packet.acknum % WINDOW_SIZE;
+    int idx = packet.acknum % SEQSPACE;
     if (!A_ack[idx]) {
         A_ack[idx] = 1;
-        while (A_ack[A_base % WINDOW_SIZE]) {
-            A_ack[A_base % WINDOW_SIZE] = 0;
-            A_buffer[A_base % WINDOW_SIZE].seqnum = -1;
-            A_base = (A_base + 1) % (WINDOW_SIZE * 2);
+        while (A_ack[A_base % SEQSPACE]) {
+            A_ack[A_base % SEQSPACE] = 0;
+            A_buffer[A_base % SEQSPACE].seqnum = -1;
+            A_base = (A_base + 1) % SEQSPACE;
         }
         stoptimer(0);
         if (A_base != A_nextseq) {
@@ -94,7 +97,7 @@ void A_timerinterrupt() {
 
 void B_init() {
     int i;
-    for (i = 0; i < WINDOW_SIZE; i++) {
+    for (i = 0; i < SEQSPACE; i++) {
         B_received[i] = 0;
         B_buffer[i].seqnum = -1;
     }
@@ -106,18 +109,19 @@ void B_input(struct pkt packet) {
     if (checksum != packet.checksum) return;
 
     int seq = packet.seqnum;
-    int idx = seq % WINDOW_SIZE;
+    int idx = seq % SEQSPACE;
 
-    if (seq >= B_expected && seq < B_expected + WINDOW_SIZE) {
+    if ((seq >= B_expected && seq < B_expected + WINDOW_SIZE) ||
+        (B_expected + WINDOW_SIZE >= SEQSPACE && seq < (B_expected + WINDOW_SIZE) % SEQSPACE)) {
         if (!B_received[idx]) {
             B_received[idx] = 1;
             B_buffer[idx] = packet;
         }
 
-        while (B_received[B_expected % WINDOW_SIZE]) {
-            tolayer5(1, B_buffer[B_expected % WINDOW_SIZE].payload);
-            B_received[B_expected % WINDOW_SIZE] = 0;
-            B_expected = (B_expected + 1) % (WINDOW_SIZE * 2);
+        while (B_received[B_expected % SEQSPACE]) {
+            tolayer5(1, B_buffer[B_expected % SEQSPACE].payload);
+            B_received[B_expected % SEQSPACE] = 0;
+            B_expected = (B_expected + 1) % SEQSPACE;
         }
     }
 
