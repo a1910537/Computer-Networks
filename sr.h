443bc980ce0fd5ff614283ce19817d8c3811f498
1999051 <141125113+1999051@users.noreply.github.com>
2025-05-05 01:24:08 +0930

    Update sr.h

--- sr.h
+++ sr.h
@@ -1,37 +1,15 @@
 #ifndef SR_H
 #define SR_H
 
-#include "emulator.h"
-
-#define RTT  16.0       /* round trip time. MUST BE SET TO 16.0 when submitting assignment */
-#define WINDOWSIZE 6    /* maximum number of buffered unacknowledged packets */
-#define SEQSPACE 12     /* sequence space should be at least 2 * WINDOWSIZE */
-#define NOTINUSE (-1)   /* value used for unused header fields */
-
-/* Sender (A) function declarations */
-void A_output(struct msg message);
-void A_input(struct pkt packet);
-void A_timerinterrupt(void);
 void A_init(void);
-
-/* Receiver (B) function declarations */
-void B_input(struct pkt packet);
 void B_init(void);
+void A_input(struct pkt);
+void B_input(struct pkt);
+void A_output(struct msg);
+void A_timerinterrupt(void);
 
-/* Optional (unused for simplex) */
-void B_output(struct msg message);
+#define BIDIRECTIONAL 0
+void B_output(struct msg);
 void B_timerinterrupt(void);
 
-/* Utility functions */
-int ComputeChecksum(struct pkt packet);
-bool IsCorrupted(struct pkt packet);
-
-/* External statistics variables (defined in emulator.c) */
-extern int TRACE;
-extern int packets_resent;
-extern int packets_received;
-extern int total_ACKs_received;
-extern int new_ACKs;
-extern int window_full;
-
-#endif // SR_H
+#endif
