366f949d67406a31c53f6d8a4b4741f374359335
1999051 <141125113+1999051@users.noreply.github.com>
2025-05-05 01:53:06 +0930

    Update sr.h

--- sr.h
+++ sr.h
@@ -1,24 +1,17 @@
 #ifndef SR_H
 #define SR_H
 
-/* 前置声明，避免重复定义结构体 */
-struct msg;
-struct pkt;
-
-/* A 端接口 */
 void A_init(void);
 void A_output(struct msg message);
 void A_input(struct pkt packet);
 void A_timerinterrupt(void);
 
-/* B 端接口 */
 void B_init(void);
 void B_input(struct pkt packet);
 void B_output(struct msg message);
 void B_timerinterrupt(void);
 
-/* 工具函数 */
 int ComputeChecksum(struct pkt packet);
-int IsCorrupted(struct pkt packet);  // 注意：如果你在 .c 里用的是 bool，请改成 int 返回 0/1
+int IsCorrupted(struct pkt packet);
 
-#endif /* SR_H */
+#endif
