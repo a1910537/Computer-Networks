65cc9d31f62e1081711016f8a0589a8d186509ef
1999051 <141125113+1999051@users.noreply.github.com>
2025-05-05 01:32:57 +0930

    Update sr.h

--- sr.h
+++ sr.h
@@ -1,15 +1,25 @@
 #ifndef SR_H
 #define SR_H
 
+struct msg {
+  char data[20];
+};
+
+struct pkt {
+  int seqnum;
+  int acknum;
+  int checksum;
+  char payload[20];
+};
+
 void A_init(void);
-void B_init(void);
-void A_input(struct pkt);
-void B_input(struct pkt);
-void A_output(struct msg);
+void A_output(struct msg message);
+void A_input(struct pkt packet);
 void A_timerinterrupt(void);
 
-#define BIDIRECTIONAL 0
-void B_output(struct msg);
+void B_init(void);
+void B_input(struct pkt packet);
+void B_output(struct msg message);
 void B_timerinterrupt(void);
 
 #endif
