09c21b6e8ff69d0b6f7e26729699e50fc4e099a7
1999051 <141125113+1999051@users.noreply.github.com>
2025-05-05 01:34:13 +0930

    Update sr.h

--- sr.h
+++ sr.h
@@ -1,16 +1,7 @@
 #ifndef SR_H
 #define SR_H
 
-struct msg {
-  char data[20];
-};
-
-struct pkt {
-  int seqnum;
-  int acknum;
-  int checksum;
-  char payload[20];
-};
+#include "emulator.h"
 
 void A_init(void);
 void A_output(struct msg message);
@@ -19,7 +10,7 @@ void A_timerinterrupt(void);
 
 void B_init(void);
 void B_input(struct pkt packet);
-void B_output(struct msg message);
-void B_timerinterrupt(void);
+void B_output(struct msg message);  // 如果不实现也可以留空
+void B_timerinterrupt(void);        // 如果不实现也可以留空
 
 #endif
